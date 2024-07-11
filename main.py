b33937c2169b3e3dea469786b227cb807f0a9e8d
Muhammad-006 <Hamza.Bilal.Nadeem.2017@gmail.com>
2024-07-11 13:34:25 +0500

    Caesar-Cipher

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
+import art
+print(art.logo)
+check = 'yes'
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-import random
-import hangman_words
-import hangman_art
+def caesar(word, shifts, direction):
+  new_word = ''
+  for i in range(0 , len(word)):
+    if word[i] not in alphabet:
+      new_word += word[i]
+      continue
+    for k in range (0 , 26):
+      if alphabet[k] == word[i]:
+        if direction == 'encode':
+          if (k + shift) >= 26:
+            new_word += (''.join(alphabet[k + shift - 26]))   
+          else:
+            new_word += (''.join(alphabet[k + shift]))
+        elif direction == 'decode':
+          if (k - shift) < 0:
+            new_word += (''.join(alphabet[k - shift + 26]))   
+          else:
+            new_word += (''.join(alphabet[k - shift]))
+      
+  print(f"The {direction}ed text is {new_word}")
 
-print(hangman_art.logo)
-chosen_word = (random.choice(hangman_words.word_list)).lower()
-
-
-used_letters = ''
-lives = 6
-check = True
-display = []
-
-for letter in chosen_word:
-    display += '_'
-print(f"\n\t\t\t\t{' '.join(display)}\n")
-
-while check:
-    guess = input("Guess a letter: ").lower()
-    
-    if guess in used_letters:
-        print(f"You have already guessed {guess}.\n")
-        print(hangman_art.stages[lives])
-        continue
-            
-    i = 0
-    for letter in chosen_word:
-        if letter == guess:
-            display[i] = letter
-        i += 1 
-    
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"OOPs! Your guessed letter {guess} is not in the word.")
-        if lives == 0:
-            check = False
-            print("\nOh! You lose.\n")
-            print("\nThe word was:")
-            print(f"\t\t\t{' '.join(chosen_word)}\n")
-            
-    if check != False:
-        print(f"\n\t\t\t\t{' '.join(display)}\n")
-
-    if '_' not in display:
-        check = False
-        print("Congratulations! You Won.")
-    
-    used_letters += guess
-    print(hangman_art.stages[lives])
-    
+while check == 'yes':
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  if direction == 'encode' or direction == 'decode':
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > 26:
+      while shift > 26:
+        shift -= 26
+    elif shift < 0:
+      while shift < 0:
+        shift += 26
+    caesar(word = text, shifts = shift, direction = direction)
+  check = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()

483feee563e2993e76a47c1bae1c37493f76cebc
Muhammad Bilal <MBilal.Nadeem236@gmail.com>
2024-07-31 10:04:09 +0500

    Update scoreboard.py

--- Snake-Game/scoreboard.py
+++ Snake-Game/scoreboard.py
@@ -11,17 +11,24 @@ class ScoreBoard(Turtle):
         self.up()
         self.goto(0,290)
         self.score = 0
+        with open('highestscore.txt', mode='r') as file:
+            self.highest_score = int(file.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f'Score: {self.score}', False, align= ALLIGN, font= FONT)
+        self.clear()
+        self.write(f'Score: {self.score} | Highest score: {self.highest_score}', False,
+                   align= ALLIGN, font= FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f'GAME OVER.', False, align= ALLIGN, font= FONT)
-
+    def reset_game(self):
+        if self.highest_score < self.score:
+            self.highest_score = self.score
+            with open('highestscore.txt', mode='w') as file:
+                file.write(f'{self.highest_score}')
+        self.score = 0
+        self.goto(0, 290)
+        self.update_scoreboard()
